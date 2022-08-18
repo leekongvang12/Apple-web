3a7dbb17ebd70bab915cb2af5875b387b69562e5
SaiyaladPS <jayj28222@gmail.com>
2022-08-19 00:03:18 +0700

    up projec apple

---
+++ js/button.js
@@ -0,0 +1,19 @@
+const box = document.getElementById("box");
+const bars2 = document.getElementById("sou")
+const bars = document.getElementById("lee");
+
+function jay(){
+    box.classList.toggle("x-box-22");
+}
+// function lee
+
+function lee(){
+    bars.classList.toggle("see")
+}
+
+function vor(){
+    bars2.classList.toggle("img")
+}
+function btn(){
+    alert("ທ່ານໄດ້ສັ່ງຊື້ແລ້ວ ຂອບໃຈທີ່ໃຊ້ບໍລິການ")
+}
